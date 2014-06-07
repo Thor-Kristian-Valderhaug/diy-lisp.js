b6d42455733654820eb9bf128a364293c78792b7
Alexander Teinum <ateinum@gmail.com>
2014-06-07 12:36:39 +0200

    Refactor

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
-module.exports.parse = (str) => {
+function isBoolean(str) {
+  return str === '#t' || str === '#f';
+}
+
+function parseBoolean(str) {
   switch (str) {
     case '#t':
       return true;
     case '#f':
       return false;
-    default:
-      if (isNaN(parseInt(str)) === true)
-        return str;
-      else
-        return parseInt(str);
   }
+}
+
+function isInteger(str) {
+  return isNaN(parseInt(str)) === false;
+}
+
+function parseInteger(str) {
+  return parseInt(str);
+}
+
+module.exports.parse = (str) => {
+  if (isBoolean(str) === true)
+    return parseBoolean(str);
+  else if (isInteger(str) === true)
+    return parseInteger(str);
+  else
+    return str;
 };
