cf1843640b773893db415093033896c4521e5566
Alexander Teinum <ateinum@gmail.com>
2014-06-07 11:59:11 +0200

    Rename test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var lisp = require('../src/app.js');
 
-test('foo', function(t) {
+test('parse single symbol', function(t) {
   t.plan(1);
   t.equal(lisp.parse('foo'), 'foo');
 });
